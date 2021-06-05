f28f25186a6fa40f2efd2942e98c810d6a142f32
devashish3382 <deva3382@hotmail.com>
2021-06-06 00:19:34 +0530

    connection paramters made configurable,removed momentjs

--- src/DB/ConnectDB.js
+++ src/DB/ConnectDB.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection(
   {
-    user:"root",
-    password:'Deva947%',
-    host:"localhost",
-    port:3306,
-    database:"subscriptionservice"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT,
+    database: "subscriptionservice"
   }
 )
-connection.connect((err)=>{
-  if(err)
-  throw new Error("Could not connect to DB");
+connection.connect((err) => {
+  if (err)
+    throw new Error("Could not connect to DB");
 })
-module.exports  = connection;
+module.exports = connection;
